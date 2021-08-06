fb2e9ac269655cd1b1a74057548ebee9393d029b
MagdaBer <magda.berger@gmx.de>
2021-08-06 11:55:18 +0200

    ✨ Add main element and to child elements, h1 and input

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,17 @@ import { createElement } from './utils/createElement';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
+const mainElement = createElement('main', {
+  childElements: [
+    createElement('h1', {
+      innerText: 'Animal Crossing: New Horizon',
+    }),
+    createElement('input', {
+      placeholder: 'Find your favorite bug',
+    }),
+  ],
+});
+
 if (app !== null) {
-  app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+  app.append(mainElement);
 }
